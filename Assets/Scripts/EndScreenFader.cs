2968c8386c56372f76f268e2cd18191e6df62cc4
= <bretsmith1124@gmail.com>
2025-05-26 15:46:02 -0500

    logo

--- Assets/Scripts/EndScreenFader.cs
+++ Assets/Scripts/EndScreenFader.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 
 public class EndScreenFader : MonoBehaviour
 {
-    [SerializeField] private CanvasGroup fadeGroup;
+    //[SerializeField] private CanvasGroup fadeGroup;
     [SerializeField] private GameObject theEndText;
     [SerializeField] private float fadeDuration = 2f;
     [SerializeField] private float delayBeforeText = 1f;
 
     void Start()
     {
-        fadeGroup.alpha = 0f;
+        //fadeGroup.alpha = 0f;
         theEndText.SetActive(false);
     }
 
@@ -21,17 +21,20 @@ public class EndScreenFader : MonoBehaviour
 
     private IEnumerator FadeToEnd()
     {
-        float t = 0f;
-        while (t < fadeDuration)
-        {
-            t += Time.deltaTime;
-            fadeGroup.alpha = Mathf.Lerp(0f, 1f, t / fadeDuration);
-            yield return null;
-        }
+        //float t = 0f;
+        //while (t < fadeDuration)
+        //{
+        //    t += Time.deltaTime;
+        //    fadeGroup.alpha = Mathf.Lerp(0f, 1f, t / fadeDuration);
+        //    yield return null;
+        //}
 
-        fadeGroup.alpha = 1f;
+        //fadeGroup.alpha = 1f;
 
         yield return new WaitForSeconds(delayBeforeText);
         theEndText.SetActive(true);
+        Animator animator = theEndText.GetComponent<Animator>();
+        animator.Play("logo");
+        animator.StopPlayback();
     }
 }
